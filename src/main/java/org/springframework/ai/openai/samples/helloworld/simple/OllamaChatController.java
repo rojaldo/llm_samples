c605744b446efa56c1951f0be24c98c5d1666727
Daniel Rojo <danielrojo@gmail.com>
2024-05-23 16:36:02 +0200

    vector storage with qdrant

--- src/main/java/org/springframework/ai/openai/samples/helloworld/simple/OllamaChatController.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/simple/OllamaChatController.java
@@ -2,18 +2,18 @@ package org.springframework.ai.openai.samples.helloworld.simple;
 
 import java.util.Map;
 
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.ai.ollama.api.OllamaApi.ChatResponse;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -22,13 +22,15 @@ public class OllamaChatController {
 
     private final OllamaChatClient ollamaChatClient;
     private final EmbeddingClient embeddingClient;
+    private final VectorStore vectorStore;
 
 
 
     @Autowired
-    public OllamaChatController(OllamaChatClient chatClient, EmbeddingClient embeddingClient) {
+    public OllamaChatController(OllamaChatClient chatClient, EmbeddingClient embeddingClient, VectorStore vectorStore) {
         this.ollamaChatClient = chatClient;
         this.embeddingClient = embeddingClient;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/ai/generate")
@@ -64,10 +66,38 @@ public class OllamaChatController {
         return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
     }
 
-    // @GetMapping("/ai/generateStream")
-	// public Object generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-    //     Prompt prompt = new Prompt(new UserMessage(message));
-    //     return ollamaChatClient.stream(prompt);
-    // }
+    @GetMapping("/ai/ollama/vector/add")
+    public Map add() {
+        List <Document> documents = List.of(new Document("Si tienes un archivo JSON con datos que deseas cargar en la base de datos vectorial, puedes usar la clase JsonReader de Spring AI para cargar campos específicos en el JSON, dividirlos en piezas pequeñas y luego pasar esas piezas pequeñas a la implementación de VectorStore.", 
+        Map.of("title", "JSON", "author", "Other")));
+        vectorStore.add(documents);
+        return Map.of("add", "success");
+    }
+
+    @GetMapping("/ai/ollama/vector/search")
+    public Map search(@RequestParam(value = "query", defaultValue = "archivo JSON") String query) {
+        List <Document> documents = vectorStore.similaritySearch(SearchRequest.query(query));
+        return Map.of("search", documents);
+    }
+
+    @GetMapping("/ai/ollama/vector/search_query")
+    public Map search_query(@RequestParam(value = "query", defaultValue = "archivo JSON") String query,
+                            @RequestParam(value = "k", defaultValue = "1") int topK,
+                            @RequestParam(value = "threshold", defaultValue = "0.75") double threshold,
+                            @RequestParam(value = "filter", defaultValue = "author == 'Other'") String filter) {
+        List <Document> documents = vectorStore.similaritySearch(
+            SearchRequest.defaults()
+                .withQuery(query)
+                .withTopK(topK)
+                .withSimilarityThreshold(threshold)
+                .withFilterExpression(filter));
+        return Map.of("search", documents);
+    }
+
+    @DeleteMapping ("/ai/ollama/vector/delete")
+    public Map delete( @RequestParam(value = "id") String id) {
+        vectorStore.delete(List.of(id));
+        return Map.of("delete", "success");
+    }
 
 }
