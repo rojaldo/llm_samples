d07643f34640e45cfcce3a2c7efeb5851690f6a8
Daniel Rojo <danielrojo@gmail.com>
2024-05-27 18:32:28 +0200

    check answer

--- src/main/java/org/springframework/ai/openai/samples/helloworld/simple/TrivialController.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/simple/TrivialController.java
@@ -7,9 +7,14 @@ import org.springframework.ai.ollama.api.OllamaApi.Message.Role;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.models.TrivialAnswer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Map;
 import java.util.List;
@@ -20,7 +25,7 @@ public class TrivialController {
 
     private final OllamaApi oapi;
 
-    private final String ROLE_SYSTEM = 
+    private final String ROLE_SYSTEM_GENERATE = 
     """
         You are a helpful AI assistant, that only answers in just JSON format. 
         You make cards for a quiz game. 
@@ -28,6 +33,14 @@ public class TrivialController {
         The answer should be always in json format as this: { \"question\": \"question\", \"language\":<the language used in the card> \"correct_answer\": \"correct answer\", \"incorrect_answers\": [\"incorrect answer 1\", \"incorrect answer 2\", \"incorrect answer 3\"] } 
         The code for the field language should be the language code, for example, for English is "en", for Spanish is "es", etc.
         In case that there are several cards, return them as an array of json objects.
+    """;
+
+    private final String ROLE_SYSTEM_CHECK = 
+    """
+        You are a helpful AI assistant, that only answers in just JSON format. 
+        You check if the response of a quiz card is correct.
+        You receive a json object with the question and the answer given by the user. This json should be always in json format as this: { \"question\": \"question\", \"answer\": \"answer\" }
+        You respond with a json object qith the fields: "question", "answer" and "correct" with the value true or false.
     """;
 
     @Autowired
@@ -46,7 +59,7 @@ public class TrivialController {
             .withStream(false) // not streaming
             .withMessages(List.of(
                     Message.builder(Role.SYSTEM)
-                        .withContent(ROLE_SYSTEM)
+                        .withContent(ROLE_SYSTEM_GENERATE)
                         .build(),
                     Message.builder(Role.USER)
                         .withContent(topic + " in " + lang + " language")
@@ -68,7 +81,7 @@ public class TrivialController {
             .withStream(false) // not streaming
             .withMessages(List.of(
                     Message.builder(Role.SYSTEM)
-                        .withContent(ROLE_SYSTEM)
+                        .withContent(ROLE_SYSTEM_GENERATE)
                         .build(),
                     Message.builder(Role.USER)
                         .withContent(topic)
@@ -77,4 +90,29 @@ public class TrivialController {
             .build();
         return Map.of("generation", oapi.chat(request).message().content().indent(2));
     }
+
+    // post method for /ai/trivial/check 
+    // get json object with question and answer from body
+    @PostMapping(
+        value="/ai/trivial/check", 
+        consumes = {MediaType.APPLICATION_JSON_VALUE}, 
+        produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Map check( @RequestBody TrivialAnswer body) {
+
+        String requestString = "{ \"question\": \"" + body.question + "\", \"answer\": \"" + body.answer + "\" }";
+        OllamaApi oapi = new OllamaApi();
+        var request = ChatRequest.builder("llama3")
+            .withStream(false) // not streaming
+            .withMessages(List.of(
+                    Message.builder(Role.SYSTEM)
+                        .withContent(ROLE_SYSTEM_CHECK)
+                        .build(),
+                    Message.builder(Role.USER)
+                        .withContent(requestString)
+                        .build()))
+            .withOptions(OllamaOptions.create().withTemperature(0.5f))
+            .build();
+        return Map.of("generation", oapi.chat(request).message().content());
+    }
+
 }
---
+++ src/main/java/org/springframework/models/TrivialAnswer.java
@@ -0,0 +1,8 @@
+package org.springframework.models;
+
+public class TrivialAnswer {
+
+    public String question;
+    public String answer;
+    
+}
